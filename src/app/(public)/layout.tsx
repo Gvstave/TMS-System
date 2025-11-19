a6d13eaeaf8544c543eff5e05920db5c3dd2790c
Gustave Ilunga <ilungagustave73@gmail.com>
2025-11-19 06:37:06 +0000

    The home page appears to have no content

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -10,7 +10,7 @@ export default function PublicLayout({
   return (
     <div className="flex flex-col min-h-screen">
       <header className="px-4 lg:px-6 h-16 flex items-center border-b">
-        <Link href="/home" className="flex items-center justify-center gap-2" prefetch={false}>
+        <Link href="/" className="flex items-center justify-center gap-2" prefetch={false}>
           <CheckSquare className="h-6 w-6 text-primary" />
           <span className="text-xl font-bold font-headline">TMS</span>
         </Link>
@@ -27,7 +27,18 @@ export default function PublicLayout({
           </Button>
         </nav>
       </header>
-      {children}
+      <main className="flex-1">{children}</main>
+      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+        <p className="text-xs text-muted-foreground">&copy; 2024 Task Management System. All rights reserved.</p>
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
+            Terms of Service
+          </Link>
+          <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
+            Privacy
+          </Link>
+        </nav>
+      </footer>
     </div>
   );
 }
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,4 +1,3 @@
-
 import { Button } from '@/components/ui/button';
 import {
     Card,
@@ -12,8 +11,7 @@ import Link from 'next/link';
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-1">
+    <>
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
@@ -88,18 +86,6 @@ export default function LandingPage() {
                 </div>
             </div>
         </section>
-      </main>
-      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-muted-foreground">&copy; 2024 Task Management System. All rights reserved.</p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
-            Terms of Service
-          </Link>
-          <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
-            Privacy
-          </Link>
-        </nav>
-      </footer>
-    </div>
+    </>
   );
 }
